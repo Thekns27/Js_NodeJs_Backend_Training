29a9ff55ebd79319cee5dd593d2cc4624b673cf8
Thekns27 <johndoe@example.com>
2025-11-11 16:03:49 +0630

    modified app.js

--- UT_Brighter_Myanmar/Mongoose/app.js
+++ UT_Brighter_Myanmar/Mongoose/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 mongoose.connect(process.env.DB_URL);
+//mongoose.connect ('mongodb://127.0.0.1:27017/Testdb');
 
 app.use(express.json());
 
